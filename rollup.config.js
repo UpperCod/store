5c4d2ec2dd84bb88782a7cf26139d27372ba7024
uppercod <mtrujillo@outlook.cl>
2019-02-27 11:29:47 -0300

    update pkg 0.0.1

--- rollup.config.js
+++ rollup.config.js
@@ -2,30 +2,51 @@ import pkg from "./package.json";
 import buble from "rollup-plugin-buble";
 import { terser } from "rollup-plugin-terser";
 
-export default {
-    input: pkg.source,
-    output: [
-        {
-            file: pkg.main,
-            sourcemap: true,
-            format: "cjs"
-        },
-        {
-            file: pkg["umd:main"],
-            sourcemap: true,
-            format: "umd",
-            name: pkg.name
-        },
-        {
-            file: pkg["module"],
-            sourcemap: true,
-            format: "es"
-        }
-    ],
-    plugins: [
-        buble({
-            objectAssign: "Object.assign"
-        }),
-        terser()
-    ]
-};
+let plugins = [
+    buble({
+        jsx: "h",
+        objectAssign: "Object.assign"
+    }),
+    terser()
+];
+
+export default [
+    {
+        input: pkg.source,
+        output: [
+            {
+                file: pkg.main,
+                sourcemap: true,
+                format: "cjs"
+            },
+            {
+                file: pkg["umd:main"],
+                sourcemap: true,
+                format: "umd",
+                name: pkg.name
+            },
+            {
+                file: pkg["module"],
+                sourcemap: true,
+                format: "es"
+            }
+        ],
+        plugins
+    },
+    {
+        input: "components/atomico.js",
+        output: [
+            {
+                file: "components.js",
+                sourcemap: true,
+                format: "cjs"
+            },
+            {
+                file: "components.mjs",
+                sourcemap: true,
+                format: "es"
+            }
+        ],
+        plugins
+    }
+];
