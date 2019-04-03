221652b5b270caeb49c80c5aa03e4eb60a38f8ba
uppercod <mtrujillo@outlook.cl>
2019-04-03 00:52:10 -0300

    now the states can only be objects

--- src/index.js
+++ src/index.js
@@ -1,91 +1,94 @@
+export function assign(master, commit) {
+	for (let key in commit) {
+		master[key] = commit[key];
+	}
+	for (let i = 2; i < arguments.length; i++) assign(master, arguments[i]);
+	return master;
+}
+
+export function consumer(value, payload, get, set) {
+	return Promise.resolve(value).then(value => {
+		let state = get();
+		if (typeof value == "function") {
+			return consumer(value(state), payload, get, set);
+		}
+		if (typeof value == "object") {
+			if (typeof value.next == "function") {
+				return new Promise(resolve => {
+					function scan(generator) {
+						Promise.resolve(generator.next(get())).then(({ value, done }) =>
+							consumer(value, null, get, set).then(() => {
+								done ? resolve(get()) : scan(generator);
+							})
+						);
+					}
+					scan(value);
+				});
+			}
+		}
+		set(value);
+		return state;
+	});
+}
+/**
+ * @param {object} reducers
+ * @param {object} [state]
+ * @param {Function} [logger]
+ */
 export function Store(reducers, state, logger) {
-    let actions = {},
-        handlers = [];
+	let actions = {},
+		handlers = [];
 
-    function getSpace(space) {
-        return space ? state[space] : state;
-    }
-    function setSpace(space, type, value, payload) {
-        return Promise.resolve(value).then(value => {
-            let localState = getSpace(space);
-            if (typeof value === "function")
-                return setSpace(space, type, value(localState, payload));
+	state = state || {};
 
-            if (typeof value === "object") {
-                if (typeof value.next === "function") {
-                    return new Promise(resolve => {
-                        function scan(generator) {
-                            Promise.resolve(
-                                generator.next(getSpace(space))
-                            ).then(({ value, done }) =>
-                                setSpace(space, type, value).then(() => {
-                                    if (done) {
-                                        resolve(state);
-                                    } else {
-                                        scan(generator);
-                                    }
-                                })
-                            );
-                        }
-                        scan(value);
-                    });
-                }
-            }
-            if (value !== localState) {
-                if (logger) logger(space, type, localState, value);
-                if (space) {
-                    let nextState = {};
-                    for (let key in state) {
-                        nextState[key] = state[key];
-                    }
-                    nextState[space] = value;
-                    value = nextState;
-                }
-                state = value;
-                let length = handlers.length;
-                for (let i = 0; i < length; i++) handlers[i](state, space);
-            }
-            return state;
-        });
-    }
+	function emit(space) {
+		let length = handlers.length;
+		for (let i = 0; i < length; i++) handlers[i](state, space);
+	}
 
-    function createAction(space, type, reducer) {
-        return payload => setSpace(space, type, reducer, payload);
-    }
+	function createAction(space, type, reducer) {
+		let get = () => (space ? state[space] : state),
+			set = value => {
+				let localState = get();
+				if (localState == state) return;
+				if (logger) logger(space, type, localState, value);
+				if (space) value = { [space]: assign({}, localState, value) };
+				state = assign({}, state, value);
+				emit(space);
+			};
+		if (space && !(space in state)) state[space] = {};
+		return payload => consumer(reducer, payload, get, set);
+	}
 
-    function setActions(reducers, parent) {
-        for (let space in reducers) {
-            let reducer = reducers[space];
-            if (typeof reducer === "object") {
-                for (let type in reducer) {
-                    actions[space] = actions[space] || {};
-                    actions[space][type] = createAction(
-                        space,
-                        type,
-                        reducer[type]
-                    );
-                }
-            } else {
-                actions[space] = createAction(null, space, reducer);
-            }
-        }
-    }
+	function setActions(reducers, parent) {
+		for (let space in reducers) {
+			let reducer = reducers[space];
+			if (typeof reducer == "object") {
+				for (let type in reducer) {
+					actions[space] = actions[space] || {};
+					actions[space][type] = createAction(space, type, reducer[type]);
+				}
+			} else {
+				actions[space] = createAction(null, space, reducer);
+			}
+		}
+	}
 
-    function subscribe(handler, space) {
-        handlers.push(handler);
-        return function unsubscribe() {
-            handlers.splice(handlers.indexOf(handler) >>> 0, 1);
-        };
-    }
+	function subscribe(handler, space) {
+		handlers.push(handler);
+		return function unsubscribe() {
+			handlers.splice(handlers.indexOf(handler) >>> 0, 1);
+		};
+	}
 
-    setActions(reducers);
+	setActions(reducers);
 
-    return {
-        setActions,
-        actions,
-        subscribe,
-        get state() {
-            return state;
-        }
-    };
+	return {
+		setActions,
+		actions,
+		subscribe,
+		get state() {
+			return state;
+		}
+	};
 }
