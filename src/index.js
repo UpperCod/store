0a2b59d2b9aaddffb1059801a689fd3a57050139
uppercod <mtrujillo@outlook.cl>
2019-04-03 02:13:19 -0300

    rewrite

--- src/index.js
+++ src/index.js
@@ -1,94 +1,58 @@
-export function assign(master, commit) {
-	for (let key in commit) {
-		master[key] = commit[key];
-	}
-	for (let i = 2; i < arguments.length; i++) assign(master, arguments[i]);
-	return master;
+import { assign, consumer } from "./utils";
+
+let LOG = false;
+
+export function setLog(log) {
+	LOG = log;
 }
 
-export function consumer(value, payload, get, set) {
-	return Promise.resolve(value).then(value => {
-		let state = get();
+export class Store {
+	constructor(state) {
+		this.state = state || {};
+		this.actions = {};
+		this.handlers = [];
+		let { actions } = this.constructor;
+		this.setActions(actions);
+	}
+	dispatch(action) {
+		let { handlers } = this,
+			length = handlers.length;
+		for (let i = 0; i < length; i++) handlers[i](this.state, action);
+	}
+	setActions(actions) {
+		for (let name in actions) this.setAction(name, actions[name]);
+	}
+	setAction(name, value) {
+		let set = value => {
+			if (LOG) LOG(this, name, value);
+			this.state = assign({}, this.state, value);
+			this.dispatch(name);
+		};
+
 		if (typeof value == "function") {
-			return consumer(value(state), payload, get, set);
-		}
-		if (typeof value == "object") {
-			if (typeof value.next == "function") {
-				return new Promise(resolve => {
-					function scan(generator) {
-						Promise.resolve(generator.next(get())).then(({ value, done }) =>
-							consumer(value, null, get, set).then(() => {
-								done ? resolve(get()) : scan(generator);
-							})
-						);
-					}
-					scan(value);
-				});
-			}
-		}
-		set(value);
-		return state;
-	});
-}
-/**
- * @param {object} reducers
- * @param {object} [state]
- * @param {Function} [logger]
- */
-export function Store(reducers, state, logger) {
-	let actions = {},
-		handlers = [];
-
-	state = state || {};
-
-	function emit(space) {
-		let length = handlers.length;
-		for (let i = 0; i < length; i++) handlers[i](state, space);
-	}
-
-	function createAction(space, type, reducer) {
-		let get = () => (space ? state[space] : state),
-			set = value => {
-				let localState = get();
-				if (localState == state) return;
-				if (logger) logger(space, type, localState, value);
-				if (space) value = { [space]: assign({}, localState, value) };
-				state = assign({}, state, value);
-				emit(space);
-			};
-		if (space && !(space in state)) state[space] = {};
-		return payload => consumer(reducer, payload, get, set);
-	}
-
-	function setActions(reducers, parent) {
-		for (let space in reducers) {
-			let reducer = reducers[space];
-			if (typeof reducer == "object") {
-				for (let type in reducer) {
-					actions[space] = actions[space] || {};
-					actions[space][type] = createAction(space, type, reducer[type]);
-				}
+			let action, instance;
+			if (value.prototype instanceof Store) {
+				instance = new value(this.state[name]);
+			} else if (value instanceof Store) {
+				instance = value;
 			} else {
-				actions[space] = createAction(null, space, reducer);
+				action = payload => {
+					return consumer(value, payload, () => this.state, set);
+				};
 			}
+			if (instance) {
+				action = instance.actions;
+				instance.subscribe(value => set({ [name]: value }));
+				this.state[name] = instance.state;
+			}
+			this.actions[name] = action;
 		}
 	}
-
-	function subscribe(handler, space) {
+	subscribe(handler) {
+		let { handlers } = this;
 		handlers.push(handler);
-		return function unsubscribe() {
+		return () => {
 			handlers.splice(handlers.indexOf(handler) >>> 0, 1);
 		};
 	}
-
-	setActions(reducers);
-
-	return {
-		setActions,
-		actions,
-		subscribe,
-		get state() {
-			return state;
-		}
-	};
 }
---
+++ src/utils.js
@@ -0,0 +1,32 @@
+export function assign(master, commit) {
+	for (let key in commit) {
+		master[key] = commit[key];
+	}
+	for (let i = 2; i < arguments.length; i++) assign(master, arguments[i]);
+	return master;
+}
+
+export function consumer(value, payload, get, set) {
+	return Promise.resolve(value).then(value => {
+		let state = get();
+		if (typeof value == "function") {
+			return consumer(value(state), payload, get, set);
+		}
+		if (typeof value == "object") {
+			if (typeof value.next == "function") {
+				return new Promise(resolve => {
+					function scan(generator) {
+						Promise.resolve(generator.next(get())).then(({ value, done }) =>
+							consumer(value, null, get, set).then(() => {
+								done ? resolve(get()) : scan(generator);
+							})
+						);
+					}
+					scan(value);
+				});
+			}
+		}
+		set(value);
+		return state;
+	});
+}
